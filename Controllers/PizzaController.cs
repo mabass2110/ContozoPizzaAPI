e33c56503fb4a6bb1fa7af0de33af36321d48e7b
helder mabasso <hfmabasso@gmail.com>
2024-07-20 19:29:59 +0200

    Implement CRUD operations

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ContosoPizza.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("pizza")]
 public class PizzaController : ControllerBase
 {
     public PizzaController()
@@ -15,7 +15,7 @@ public class PizzaController : ControllerBase
 
     // GET all action
     [HttpGet]
-public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
     // GET by Id action
     [HttpGet("{id}")]
@@ -25,9 +25,46 @@ public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
             return NotFound();
        return pizza;
        }
-    // POST action
-
     // PUT action
 
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            // This code will update the pizza and return a result
+            if (id != pizza.Id)
+                return BadRequest();
+                
+            var existingPizza = PizzaService.Get(id);
+            if(existingPizza is null)
+                return NotFound();
+        
+            PizzaService.Update(pizza);           
+        
+            return NoContent();
+        }
+
+    // POSR action
+            [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {            
+            // This code will save the pizza and return a result
+               PizzaService.Add(pizza);
+                return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
     // DELETE action
+
+            [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // This code will delete the pizza and return a result
+             var pizza = PizzaService.Get(id);
+   
+                if (pizza is null)
+                    return NotFound();
+                
+                PizzaService.Delete(id);
+            
+                return NoContent(); ;   
+        }
 }
